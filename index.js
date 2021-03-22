ba17ac29a7fc1f492df3a53ddcd949294b584bb2
Mari-8 <ryanaltimari8@gmail.com>
2021-03-21 20:48:20 -0500

    commit tree 3d4bcb09aece878fd2296e63e056dca017535553

--- index.js
+++ index.js
@@ -20,3 +20,77 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(empData) {
+    const emp = {
+        firstName: empData[0], 
+        familyName: empData[1], 
+        title: empData[2], 
+        payPerHour: empData[3], 
+        timeInEvents: [], 
+        timeOutEvents: [] 
+    }
+    return emp
+}
+
+function createEmployeeRecords(emps) {
+    let newEmps = [] 
+    for (let i=0; i < emps.length; i++) {
+        let newEmp = createEmployeeRecord(emps[i])
+        newEmps.push(newEmp)
+    }
+    return newEmps
+}
+
+function createTimeInEvent(dateNTime) {
+    const timeIn = {
+        type: "TimeIn", 
+        date: dateNTime.split(" ")[0], 
+        hour: parseInt(dateNTime.split(" ")[1])
+    }
+    this.timeInEvents.push(timeIn)
+    
+    return this
+}
+
+function createTimeOutEvent(dateNTime) {
+    const timeOut = {
+        type: "TimeOut", 
+        date: dateNTime.split(" ")[0], 
+        hour: parseInt(dateNTime.split(" ")[1])
+    }
+    this.timeOutEvents.push(timeOut) 
+
+    return this
+}
+
+function hoursWorkedOnDate(dateNTime) {
+    let timeIn = this.timeInEvents.find(function(event) {
+        return event.date === dateNTime
+    })
+    let timeOut = this.timeOutEvents.find(function(event) {
+        return event.date === dateNTime
+    })
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(dateNTime) {
+    let hoursWorked = hoursWorkedOnDate.call(this, dateNTime)
+    let hoursParsed = parseInt(hoursWorked)
+    return (this.payPerHour * hoursParsed)
+}
+
+function findEmployeeByFirstName(emps, name) {
+    return emps.find(emp => emp.firstName === name)
+}
+const reducer = (accum, currVal) => accum + currVal
+
+function calculatePayroll(emps) {
+    let total = [] 
+
+    for(let i=0; i < emps.length; i++) {
+        let partial = allWagesFor.call(emps[i]) 
+        total.push(partial)
+    }
+    return total.reduce(reducer, 0)
+}
